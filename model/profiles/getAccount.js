dd93d8d44ca0f527fe8a847ab2d70fc292c17991
Fahrizal Septrianto <fahrizalseptrianto@gmail.com>
2016-04-23 14:04:45 +0700

    ver 1.10.7 fix getAccount requirements

--- model/profiles/getAccount.js
+++ model/profiles/getAccount.js
@@ -19,7 +19,14 @@ getAccount.prototype.handleRoutes = function(router,connection){
           res.json({"message":"err.. error on selecting"});
         }else{
           if(rows.length>0){
-            res.json(rows);
+            // res.json(rows);
+            connection.query("select company_name,about,handphone,location,address,img_base64 from `host` where email = '"+email+"'",function(err,rows){
+              if(err){
+                res.json({"message":"err.. error on selecting"});
+              }else{
+                res.json(rows);
+              }
+            });
           }else{
             res.json({"message":"err.. no rows"});
           }
