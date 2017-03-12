314584eb48e98e361f62e7c8fe6b092783b43c3f
Fahrizal Septrianto <fahrizalseptrianto@gmail.com>
2017-03-12 19:04:01 +0700

    +idTipe /getAccount : con.release on addNewPorto

--- model/portofolios/addNewPortofolio.js
+++ model/portofolios/addNewPortofolio.js
@@ -31,21 +31,21 @@ addNewPortofolio.prototype.handleRoutes = function(router,pool,config){
           var description = connection.escape(req.body.description);
           var imgBase64 = connection.escape(req.body.imgbase64);
           if(sessionCode == null || sessionCode==undefined || sessionCode==''){
-            connection.end();
+            connection.release();
             res.json({"message":"err.. no params sess rec","error":"error"});
           }else{
             if(title==null || title==undefined || title==''){
-              connection.end();
+              connection.release();
               res.json({"message":"err.. no params title received","error":"error"});
             }else{
               if(description==null || description==undefined || description==''){
-                connection.end();
+                connection.release();
                 res.json({"message":"err.. no params desc received","error":"error"});
               }else{
                 var qSelectSes = "select session_host.id_host as id_host,host.email as email from `session_host` join `host` on session_host.id_host=host.id_host where session_code="+sessionCode+"";
                 connection.query(qSelectSes,function(err,rows){
                   if(err){
-                    connection.end();
+                    connection.release();
                     res.json({"message":"err.. error in selecting host from session","error":"error","qSelectSes":qSelectSes});
                   }else{
                     if(rows.length>0){
@@ -55,21 +55,21 @@ addNewPortofolio.prototype.handleRoutes = function(router,pool,config){
                       var qMax = "select count(id_portofolio) as count from `portofolio` where id_host="+idHost;
                       connection.query(qMax,function(err,rows){
                         if(err){
-                          connection.end();
+                          connection.release();
                           res.json({"message":"err.. error on count portofolio","error":"error"});
                         }else{
                           var count = rows[0].count;
                           if(count < 12){
                             connection.beginTransaction(function(err){
                               if (err) {
-                                connection.end();
+                                connection.release();
                                 res.json({"message":"err.. error on beginTransaction","error":"error","objErr":err});
                                 return;
                               }
                               var query = "insert into `portofolio` (id_host,title,description) values("+idHost+","+title+","+description+")";
                               connection.query(query,function(err,rows){
                                 if(err){
-                                  connection.end();
+                                  connection.release();
                                   res.json({"message":"err.. error in inserting new portofolio","query":query,"error":"error"});
                                 }else{
                                   if(imgBase64 != null && imgBase64 != undefined && imgBase64 != ''){
@@ -86,13 +86,13 @@ addNewPortofolio.prototype.handleRoutes = function(router,pool,config){
                                           var path = baseDocumentRoot+"assets/img/"+email+"/portofolios";
                                           var split1 = req.body.imgbase64.split(";");
                                           if(split1.length <= 1){
-                                            connection.end();
+                                            connection.release();
                                             res.json({"message":"err.. error imgbase64 invalid format sp1","error":"error"});
                                             return;
                                           }
                                           var split2 = split1[0].split("/");
                                           if(split2.length <= 1){
-                                            connection.end();
+                                            connection.release();
                                             res.json({"message":"err.. error imgbase64 invalid format sp2","error":"error"});
                                             return;
                                           }
@@ -102,7 +102,7 @@ addNewPortofolio.prototype.handleRoutes = function(router,pool,config){
                                             if(err){
                                               // console.log("message err.. error on sync");
                                               connection.rollback(function(){
-                                                connection.end();
+                                                connection.release();
                                                 res.json({"message":"err.. error on sync","error":"error","objErr":err});
                                                 return;
                                               });
@@ -111,7 +111,7 @@ addNewPortofolio.prototype.handleRoutes = function(router,pool,config){
                                                 if (err) {
                                                   // console.log("message err.. error on mkpath");
                                                   connection.rollback(function(){
-                                                    connection.end();
+                                                    connection.release();
                                                     res.json({"message":"err.. error on mkpath","error":"error","objErr":err});
                                                     return;
                                                   });
@@ -127,7 +127,7 @@ addNewPortofolio.prototype.handleRoutes = function(router,pool,config){
                                             if(err){
                                               // console.log("message err.. error in fs.write err:"+err);
                                               connection.rollback(function(){
-                                                connection.end();
+                                                connection.release();
                                                 res.json({"message":"err.. error in fs.write","error":"error","objErr":err});
                                                 return;
                                               });
@@ -139,7 +139,7 @@ addNewPortofolio.prototype.handleRoutes = function(router,pool,config){
                                               connection.query("update `portofolio` set img_base64='"+imgbase64_database+"' where id_portofolio="+idPortofolio,function(err,rows){
                                                 if(err){
                                                   connection.rollback(function(){
-                                                    connection.end();
+                                                    connection.release();
                                                     res.json({"message":"err.. error on updating host with img","error":"error","objErr":err});
                                                     return;
                                                   });
@@ -151,7 +151,7 @@ addNewPortofolio.prototype.handleRoutes = function(router,pool,config){
                                                   connection.query("update `session_host` set last_activity='"+myTimestamp+"' where session_code="+sessionCode+"",function(err,rows){
                                                     if(err){
                                                       connection.rollback(function(){
-                                                        connection.end();
+                                                        connection.release();
                                                         res.json({"message":"err.. error on update session last activity","error":"error","objErr":err});
                                                         return;
                                                       });
@@ -160,12 +160,12 @@ addNewPortofolio.prototype.handleRoutes = function(router,pool,config){
                                                       connection.commit(function(err) {
                                                         if (err) {
                                                           connection.rollback(function() {
-                                                            connection.end();
+                                                            connection.release();
                                                             res.json({"message":"err.. error commiting transaction","error":"error","objErr":err});
                                                             return;
                                                           });
                                                         }else{
-                                                          connection.end();
+                                                          connection.release();
                                                           res.json({"message":"success inserting new portofolio and updating last_activity","error":"success"});
                                                         }
                                                       });
@@ -188,7 +188,7 @@ addNewPortofolio.prototype.handleRoutes = function(router,pool,config){
                                     connection.query("update `session_host` set last_activity='"+timestamp+"' where session_code="+sessionCode+"",function(err,rows){
                                       if(err){
                                         connection.rollback(function(){
-                                          connection.end();
+                                          connection.release();
                                           res.json({"message":"err.. error on updating session","error":"error","objErr":err});
                                           return;
                                         });
@@ -196,12 +196,12 @@ addNewPortofolio.prototype.handleRoutes = function(router,pool,config){
                                         connection.commit(function(err) {
                                           if (err) {
                                             connection.rollback(function() {
-                                              connection.end();
+                                              connection.release();
                                               res.json({"message":"err.. error commiting transaction","error":"error","objErr":err});
                                               return;
                                             });
                                           }else{
-                                            connection.end();
+                                            connection.release();
                                             res.json({"message":"success inserting new portofolio and updating last_activity","error":"success"});
                                           }
                                         });
@@ -213,14 +213,14 @@ addNewPortofolio.prototype.handleRoutes = function(router,pool,config){
                             });
                             // here
                           }else{
-                            connection.end();
+                            connection.release();
                             res.json({"error":"error","message":"maximum limit portofolio exceeded","idProduct":null,"sign":12});
                           }
                         }
                       })
                       // HERE UP VALIDATION MAXIMUM 12 Portofolio
                     }else{
-                      connection.end();
+                      connection.release();
                       res.json({"message":"err.. no rows in session","error":"invalidSession"});
                     }
                   }
--- model/profiles/getAccount.js
+++ model/profiles/getAccount.js
@@ -15,14 +15,14 @@ getAccount.prototype.handleRoutes = function(router,connection){
     if(idHost == null || idHost == undefined || idHost == ''){
       res.json({"message":"err.. no params iH rec","error":"error","content":null});
     }else{
-      connection.query("select email,company_name,about,handphone,city.city_name,host.location,address,img_base64 from `host` join `city` on host.location=city.id_city where id_host = "+idHost+"",function(err,rows){
+      connection.query("select id_tipe,email,company_name,about,handphone,city.city_name,host.location,address,img_base64 from `host` join `city` on host.location=city.id_city where id_host = "+idHost+"",function(err,rows){
         if(err){
           res.json({"message":"err.. error on selecting","error":"error","content":null});
         }else{
           if(rows.length>0){
             res.json({"message":"success get account info #"+idHost,"error":"success","content":rows});
           }else{
-            connection.query("select email,company_name,about,handphone,location,address,img_base64 from `host` where id_host = "+idHost+"",function(err,rows){
+            connection.query("select id_tipe,email,company_name,about,handphone,location,address,img_base64 from `host` where id_host = "+idHost+"",function(err,rows){
               if(err){
                 res.json({"message":"err.. error on selecting","error":"error","content":null});
               }else{
